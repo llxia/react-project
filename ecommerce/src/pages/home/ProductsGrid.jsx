843e875991358abb78380d74a3712f48fa0687b2
Lan Xia <lanxiawork@gmail.com>
2026-01-26 17:21:46 -0500

    Create add to cart feature

--- ecommerce/src/pages/home/ProductsGrid.jsx
+++ ecommerce/src/pages/home/ProductsGrid.jsx
@@ -1,57 +1,11 @@
-import { formatMoney } from "../../utils/money";
+import { Product } from "./Product";
 
-export function ProductsGrid({ products }) {
+export function ProductsGrid({ products, loadCart }) {
   return (
     <div className="products-grid">
       {products &&
-        products.map(({ id, name, image, rating, priceCents }) => {
-          return (
-            <div key={id} className="product-container">
-              <div className="product-image-container">
-                <img className="product-image" src={image} />
-              </div>
-
-              <div className="product-name limit-text-to-2-lines">{name}</div>
-
-              <div className="product-rating-container">
-                <img
-                  className="product-rating-stars"
-                  src="images/ratings/rating-45.png"
-                />
-                <div className="product-rating-count link-primary">
-                  {rating.stars} * 10
-                </div>
-              </div>
-
-              <div className="product-price">{formatMoney(priceCents)}</div>
-
-              <div className="product-quantity-container">
-                <select>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                </select>
-              </div>
-
-              <div className="product-spacer"></div>
-
-              <div className="added-to-cart">
-                <img src="images/icons/checkmark.png" />
-                Added
-              </div>
-
-              <button className="add-to-cart-button button-primary">
-                Add to Cart
-              </button>
-            </div>
-          );
+        products.map((product) => {
+          return <Product product={product} loadCart={loadCart} />;
         })}
     </div>
   );
