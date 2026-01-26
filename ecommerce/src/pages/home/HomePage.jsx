05f07568badd06aaddd956dbb7afe7db53bae085
Lan Xia <lanxiawork@gmail.com>
2026-01-26 15:46:24 -0500

    Separate pages

---
+++ ecommerce/src/pages/home/HomePage.jsx
@@ -0,0 +1,24 @@
+import { Header } from "../../components/Header";
+import "./HomePage.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ProductsGrid } from "./ProductsGrid";
+
+export function HomePage({ cart }) {
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header cart={cart} />
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
