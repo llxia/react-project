860a38793a50d6ed6c841ce693a546819abaef7f
Lan Xia <lanxiawork@gmail.com>
2026-02-02 09:44:46 -0500

    Create add to cart to client

---
+++ next-app/app/components/AddToCart.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log("Add to Cart");
+        }}
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
-
+import Link from "next/link";
+import ProductCart from "./components/ProductCart";
 export default function Home() {
   return (
     <main>
       <h1>hello</h1>
+      <Link href="/users">Users</Link>
+      <ProductCart />
     </main>
   );
 }
